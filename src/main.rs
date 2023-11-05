6d80623acdb006ad492009f5c56224d077d73500
Anubhab Patnaik <anubhabr50@gmail.com>
2023-11-05 19:55:50 +0530

    remove more human written code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use clap::{App, Arg};
+use clap::{App, Arg, SubCommand};
 use dough::Project;
 use paris::Logger;
+use std::env;
+use std::process;
 
 fn main() {
     let mut log = Logger::new();
@@ -10,55 +12,48 @@ fn main() {
         .author("Anubhab P, injuly")
         .about("A command-line tool to create presentations from Markdown")
         .subcommand(
-            App::new("new")
+            SubCommand::with_name("new")
                 .about("Create a new project")
                 .arg(Arg::with_name("project-name").required(true)),
         )
         .subcommand(
-            App::new("present")
+            SubCommand::with_name("present")
                 .about("Present a deck")
                 .arg(Arg::with_name("project-name").required(true)),
         )
         .get_matches();
 
-    match matches.subcommand() {
-        ("new", Some(args)) => match args.value_of("project-name") {
-            Some(project_name) => {
-                log.info(format!("Creating new project '{}'", project_name));
-                if let Ok(cwd) = std::env::current_dir() {
-                    let project = Project::new(project_name, cwd.to_str().unwrap());
-                    if let Err(err) = project.init_project() {
-                        let mut err_msg = String::from("Could not create project, error:");
-                        err_msg.push_str(&err.to_string());
-                        log.error(err_msg);
-                    }
-                } else {
-                    log.error("Failed to get current working directory");
-                    std::process::exit(1);
-                }
-            }
-            _ => {}
-        },
-        ("present", Some(args)) => match args.value_of("project-name") {
-            Some(project_name) => {
-                log.info(format!("Presenting project '{}'", project_name));
-                if let Ok(cwd) = std::env::current_dir() {
-                    let project = Project::new(project_name, cwd.to_str().unwrap());
-                    if let Err(err) = project.present() {
-                        let mut err_msg = String::from("Could not present project, error:");
-                        err_msg.push_str(&err.to_string());
-                        log.error(err_msg);
-                        std::process::exit(1);
-                    }
-                } else {
-                    log.error("Failed to get current working directory");
-                    std::process::exit(1);
-                }
-            }
-            _ => {
-                log.error("WTF");
-            }
-        },
-        _ => {} // impossible
+    if let Some(args) = matches.subcommand_matches("new") {
+        create_project(args, &mut log);
+    } else if let Some(args) = matches.subcommand_matches("present") {
+        present_project(args, &mut log);
+    }
+}
+
+fn create_project(args: &clap::ArgMatches, log: &mut Logger) {
+    let project_name = args
+        .value_of("project-name")
+        .expect("project name is required");
+    log.info(format!("Creating new project '{}'", project_name));
+    let cwd = env::current_dir().expect("Failed to get current working directory");
+    let project = Project::new(project_name, &cwd.to_str().unwrap());
+
+    if let Err(err) = project.init_project() {
+        log.error(format!("Could not create project, error: {}", err));
+        process::exit(1);
+    }
+}
+
+fn present_project(args: &clap::ArgMatches, log: &mut Logger) {
+    let project_name = args
+        .value_of("project-name")
+        .expect("project name is required");
+    log.info(format!("Presenting project '{}'", project_name));
+    let cwd = env::current_dir().expect("Failed to get current working directory");
+    let project = Project::new(project_name, &cwd.to_str().unwrap());
+
+    if let Err(err) = project.present() {
+        log.error(format!("Could not present project, error: {}", err));
+        process::exit(2);
     }
 }
