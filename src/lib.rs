28b5aa5182d9eed3abcd5330931a4f44e7eb515d
Anubhab Patnaik <anubhabr50@gmail.com>
2024-01-31 11:44:44 +0530

    documenting

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,7 @@ use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
+// All the possible navigation actions when presenting a project.
 enum NavigationAction {
     Next,
     Previous,
@@ -24,6 +25,7 @@ enum NavigationAction {
     ScrollDown,
 }
 
+// Define a struct to hold project information.
 pub struct Project {
     fs_path: std::path::PathBuf,
     template: std::path::PathBuf,
@@ -36,7 +38,7 @@ struct DoughError(String);
 // Implement `Display` for `CustomError`.
 impl fmt::Display for DoughError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Custom Error: {}", self.0)
+        write!(f, "{}", self.0)
     }
 }
 
@@ -47,14 +49,26 @@ impl std::error::Error for DoughError {
     }
 }
 
+// Implement `Project` methods.
 impl Project {
+    /// Create a new project.
+    /// # Arguments
+    /// * `name` - The name of the project.
+    /// * `workdir` - The working directory of the project.
+    /// * `template` - The template to use for the project.
+    /// # Returns
+    /// A new project instance.
+
     pub fn new(name: &str, workdir: &str, template: &str) -> Project {
+        // The project directory is located at `./projects`. It is the parent directory of all the projects.
+        // If the directory does not exist, create it.
         let project_dir = workdir.to_owned() + "/projects";
 
         if !std::path::Path::new(&project_dir).exists() {
             fs::create_dir(&project_dir).expect("Could not create projects directory");
         }
 
+        // Use the template provided to copy the template files into the project directory.
         Project {
             fs_path: std::path::Path::new(&project_dir).join(name).to_path_buf(),
             template: std::path::Path::new(workdir)
@@ -63,15 +77,21 @@ impl Project {
                 .to_path_buf(),
         }
     }
+    /// Initialize a project.
+    /// # Arguments
+    /// * `self` - The project instance.
+    /// # Returns
+    /// A result indicating whether the project was initialized successfully or not.
 
     pub fn init_project(self: &Self) -> Result<()> {
         let mut log = Logger::new();
-
+        // Create the project directory under `./projects` using the project name.
         let create_dir_result = fs::create_dir(&self.fs_path);
+
         if create_dir_result.is_err() {
             return create_dir_result;
         }
-
+        // Copy the template files into the project directory.
         if !self.template.exists() {
             return Err(std::io::Error::new(
                 std::io::ErrorKind::NotFound,
@@ -85,6 +105,7 @@ impl Project {
         } else if !self.template.join("style.yml").exists() {
             log.warn("Template does not contain a style.yml file, using default style");
 
+            // If the template does not contain a style.yml file, copy the default style.yml file
             fs::copy(
                 std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
                     .join("templates")
@@ -94,11 +115,13 @@ impl Project {
             )?;
         }
 
+        // Copy the style map used to describe the style of the slides.
         fs::copy(
             self.template.join("style.yml"),
             self.fs_path.join("style.yml"),
         )?;
 
+        // Copy all the slides from the template into the project directory.
         for i in 1u64.. {
             let file_path = self.template.join(i.to_string() + ".md");
             if !file_path.exists() {
@@ -110,6 +133,15 @@ impl Project {
 
         return Ok(());
     }
+    /// Renders the project in terminal mode.
+    /// # Arguments
+    /// * `self` - The project instance.
+    /// file_contents - The contents of the file to be rendered.
+    /// style_map - The style map used to describe the style of the slides.
+    /// render - A boolean indicating whether to render the slide or not.
+    /// lines - The number of lines to be rendered.
+    /// # Returns
+    /// A result indicating whether the project was rendered successfully or not.
 
     fn render_term(
         file_contents: &str,
@@ -117,17 +149,25 @@ impl Project {
         render: bool,
         lines: &u32,
     ) -> std::result::Result<(NavigationAction, u32), Box<dyn Error>> {
+        // Used to check whether all the lines will be rendered or will it be rendered one by one.
         let mut clear: bool = false;
 
         if style_map.get("clear").unwrap() == "true" {
             clear = true;
         }
 
+        // The slide is styled using the prettify module.
         let slide = prettify::prettify(file_contents, &style_map)?;
-
+        // The upper and lower bounds are used to determine the number of lines to be rendered.
         let (upper_bound, lower_bound) = prettify::get_bounds();
 
         let mut lines_value = *lines;
+
+        // The range of scroll is determined by the upper and lower bounds.
+        // The code handles for different terminal types.
+        // Lines value is used to determine the number of lines to be rendered.
+        // Scrolling can be done using the up and down arrow keys, or the j and k keys.
+        // It controlls the number of lines to be rendered.
 
         if let Some(terminal_style) = style_map.get("terminal") {
             if terminal_style == "warp" {
@@ -145,6 +185,8 @@ impl Project {
             }
         }
 
+        // render is used to determine whether to render a new slide or not.
+        // if render is false, the slide is being scrolled.
         if render {
             if clear {
                 lines_value = slide.lines().count() as u32;
@@ -161,6 +203,15 @@ impl Project {
         if render {
             stdout.flush()?;
         }
+        // The navigation actions are handled here.
+        // The navigation actions are:
+        // 1. Next - Move to the next slide.
+        // 2. Previous - Move to the previous slide.
+        // 3. Exit - Exit the presentation.
+        // 4. ScrollUp - Scroll up the slide.
+        // 5. ScrollDown - Scroll down the slide.
+        // 6. None - Do nothing.
+
         for c in stdin.keys() {
             match c? {
                 Key::Right | Key::Char('l') | Key::Char('L') => {
@@ -185,15 +236,28 @@ impl Project {
         return Ok((NavigationAction::None, lines_value));
     }
 
+    /// This clears the terminal.
     fn clear() {
         let mut stdout = stdout();
         write!(stdout, "\x1B[2J\x1B[1;1H").unwrap();
     }
 
+    /// Present a project in terminal mode.
+    /// # Arguments
+    /// * `self` - The project instance.
+    /// # Returns
+    /// A result indicating whether the project was presented successfully or not.
+
     pub fn present_term(self: &Self) -> std::result::Result<(), Box<dyn Error>> {
+        // The render variable is used to determine whether to render a new slide or not.
         let mut render = true;
         let mut current_slide = 1;
+        // The lines variable is used to determine the number of lines to be rendered.
         let mut lines: u32 = 0;
+
+        // The loop is used to present the slides one by one.
+        // The loop is exited when the user exits the presentation.
+        // It handles the navigation actions.
 
         loop {
             Self::clear();
@@ -214,6 +278,7 @@ impl Project {
             }
 
             let contents = fs::read_to_string(&file_path)?;
+            // The style map is used to describe the style of the slides.
             let style_content = fs::read_to_string(self.fs_path.join("style.yml"))?;
             let style_map: HashMap<String, String> = style_content
                 .lines()
@@ -225,6 +290,12 @@ impl Project {
                     ))
                 })
                 .collect();
+
+            // The navigation actions are handled here.
+            // Scrolling up decreases the number of lines to be rendered.
+            // Scrolling down increases the number of lines to be rendered.
+            // Next and previous move to the next and previous slides respectively.
+            // Exit exits the presentation.
 
             match Self::render_term(&contents, &style_map, render, &lines)? {
                 (NavigationAction::Next, _new_lines_value) => {
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,18 @@ use paris::Logger;
 use std::env;
 use std::process;
 
+/// The main function of the program.
+///
+/// This function initializes the paris logger, parses command-line arguments using `clap`, and
+/// dispatches commands based on the provided subcommands. It handles the 'new' and 'present'
+/// subcommands, creating or presenting projects accordingly, and prints help information
+/// if no valid subcommand is provided.
+
 fn main() {
+    // Initialize the logger instance.
     let mut log = Logger::new();
 
+    // Parse command-line arguments using `clap`.
     let matches = App::new("dough")
         .version("0.0.2")
         .author("fuzzymfx, injuly")
@@ -20,6 +29,7 @@ fn main() {
         ",
         )
         .subcommand(
+            // Creates a new project under the `projects` directory.
             SubCommand::with_name("new")
                 .about("Create a new project")
                 .arg(Arg::with_name("project-name").required(true))
@@ -32,6 +42,7 @@ fn main() {
                 ),
         )
         .subcommand(
+            // Presents a project in the specified mode. Defaults to terminal mode.
             SubCommand::with_name("present")
                 .about("Present a deck")
                 .arg(Arg::with_name("project-name").required(true))
@@ -46,11 +57,15 @@ fn main() {
         )
         .get_matches();
 
+    // Dispatch commands based on the provided subcommands.
     if let Some(args) = matches.subcommand_matches("new") {
+        // Create a new project
         create_project(args, &mut log);
     } else if let Some(args) = matches.subcommand_matches("present") {
+        // Present a project
         present_project(args, &mut log);
     } else {
+        // Print help information if no valid subcommand is provided.
         print!(
             "
         ~~~~~~
@@ -65,14 +80,28 @@ fn main() {
     }
 }
 
+/// Create a new project.
+/// This function creates a new project using the provided arguments.
+/// It creates a new project directory, copies the template files into it, and initializes
+/// the project.
+/// If any of these steps fail, the function prints an error message and exits with a
+/// non-zero exit code.
+/// # Arguments
+/// * `args` - The command-line arguments provided by the user.
+/// * `log` - The paris logger instance.
+
 fn create_project(args: &clap::ArgMatches, log: &mut Logger) {
+    // Get the project name from the command-line arguments.
     let project_name = args
         .value_of("project-name")
         .expect("project name is required");
 
+    // Get the template name from the command-line arguments. If no template is provided, use
+    // the default template.
     let template = args.value_of("template").unwrap_or("default");
     log.info(format!("Creating new project '{}'", project_name));
     let cwd = env::current_dir().expect("Failed to get current working directory");
+    // Create a new project instance.
     let project = Project::new(project_name, &cwd.to_str().unwrap(), template);
 
     if let Err(err) = project.init_project() {
@@ -81,10 +110,22 @@ fn create_project(args: &clap::ArgMatches, log: &mut Logger) {
     }
 }
 
+/// Present a project.
+/// This function presents a project using the provided arguments.
+/// It initializes the project, and then presents it in the specified mode.
+/// If any of these steps fail, the function prints an error message and exits with a
+/// non-zero exit code.
+/// # Arguments
+/// * `args` - The command-line arguments provided by the user.
+/// * `log` - The paris logger instance.
+
 fn present_project(args: &clap::ArgMatches, log: &mut Logger) {
+    // Get the project name from the command-line arguments.
     let project_name = args
         .value_of("project-name")
         .expect("project name is required");
+
+    // Get the presentation mode from the command-line arguments. If no mode is provided, use terminal mode.
     let mode = args.value_of("mode").unwrap_or("term"); // Default to terminal mode
 
     log.info(format!(
@@ -92,15 +133,19 @@ fn present_project(args: &clap::ArgMatches, log: &mut Logger) {
         project_name, mode
     ));
     let cwd = env::current_dir().expect("Failed to get current working directory");
+    // Create a new project instance.
     let project = Project::new(project_name, &cwd.to_str().unwrap(), "default");
 
     match mode {
+        // The HTML mode is not implemented yet, so we only support terminal mode for now. This
+        // will be updated in the future.
         "html" => {
             // if let Err(err) = project.present_html() {
             //     log.error(format!("Could not present project in HTML, error: {}", err));
             //     process::exit(3);
             // }
         }
+        // Present the project in terminal mode.
         "term" | _ => {
             if let Err(err) = project.present_term() {
                 log.error(format!(
